862307e24a9750b4407d09b4d5a9312f99547c2b
Jacob Biehler <biehlerj@gmail.com>
2020-01-22 12:28:42 -0800

    Cleanup after merging master into feature branch

--- src/reducers/model.js
+++ src/reducers/model.js
@@ -1,13 +1,5 @@
 import * as R from 'ramda'
 import * as Actions from '../actionConsts'
-<<<<<<< HEAD
-
-const initState = {}
-
-export const getModelStore = (state, modelName) => R.path(['model', modelName], state)
-
-export const getAllModelStore = (state) => R.path(['model'], state)
-=======
 import { selectTableView } from './tableView'
 
 export const PAGINATION_AMT = 20
@@ -33,7 +25,6 @@ export const getPaginatedModel = (state, modelName) => {
 }
 
 export const getAllModelStore = state => R.path(['model'], state)
->>>>>>> master
 
 const getDefaultModelStore = () => ({ order: [], values: {} })
 
@@ -53,47 +44,26 @@ const updateIndex = (state, modelName, data) => {
   newStore.order = data.map(R.prop('id'))
   for (const node of data) {
     const oldNode = R.propOr({}, node.id, oldStore.values)
-<<<<<<< HEAD
-    const updatedNode = R.mergeDeepRight(oldNode, node)
-    newStore.values[node.id] = updatedNode
-=======
     newStore.values[node.id] = R.mergeDeepRight(oldNode, node)
->>>>>>> master
   }
   return { ...state, [modelName]: newStore }
 }
 
-<<<<<<< HEAD
-export const generateModelReducer = schema => (state = initState, action) => {
-=======
 export const generateModelReducer = () => (state = initState, action) => {
->>>>>>> master
   const modelName = R.path(['payload', 'modelName'], action)
 
   switch (action.type) {
     case Actions.UPDATE_MODEL_INDEX: {
-<<<<<<< HEAD
-      return updateIndex(state, modelName, R.pathOr([], ['payload', 'data', 'result'], action))
-    }
-
-=======
       return updateIndex(
         state,
         modelName,
         R.pathOr([], ['payload', 'data', 'result'], action)
       )
     }
->>>>>>> master
     case Actions.UPDATE_MODEL_DETAIL: {
       const id = R.path(['payload', 'id'], action)
       const store = { ...R.propOr(getDefaultModelStore(), modelName, state) }
       const oldNode = R.prop(id, store.values)
-<<<<<<< HEAD
-      const newNode = R.pathOr(R.path(['payload', 'data'], action), ['payload', 'data', 'result'], action)
-
-      if (!oldNode) {
-        // node does not exist in store, add it
-=======
       const newNode = R.pathOr(
         R.path(['payload', 'data'], action),
         ['payload', 'data', 'result'],
@@ -101,26 +71,18 @@ export const generateModelReducer = () => (state = initState, action) => {
       )
 
       if (!oldNode) {
->>>>>>> master
         store.order.push(id)
       }
       store.values[id] = newNode
 
       return { ...state, [modelName]: store }
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> master
     default:
       return state
   }
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 export const selectModel = R.propOr(initState, 'model')
 export const getDetailUrl = ({ modelName, id }) => `/${modelName}/${id}`
 export const getIndexUrl = ({ modelName }) => `/${modelName}`
