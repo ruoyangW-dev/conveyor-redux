d5dfacab4388201b8eab367f2b1531be2d6d3881
myurch <36048692+myurch@users.noreply.github.com>
2020-06-07 12:33:41 -0700

    add success alert

--- src/reducers/alerts.ts
+++ src/reducers/alerts.ts
@@ -1,5 +1,5 @@
 import { initState, handleError } from '../utils/alerts'
-import { ADD_DANGER_ALERT, DISMISS_ALERT } from '../actionConsts'
+import { ADD_DANGER_ALERT, ADD_SUCCESS_ALERT, DISMISS_ALERT } from '../actionConsts'
 import { SchemaBuilder } from '@autoinvent/conveyor-schema'
 import { Reducer } from './reducer'
 import * as R from 'ramda'
@@ -11,6 +11,9 @@ export class AlertsReducer extends Reducer {
 
   [ADD_DANGER_ALERT](state: any, action: any) {
     return [...state, handleError({ payload: action.payload, type: 'danger' })]
+  }
+  [ADD_SUCCESS_ALERT](state: any, action: any) {
+    return [...state, handleError({ payload: action.payload, type: 'success' })]
   }
   [DISMISS_ALERT](state: any, action: any) {
     return state.filter(
