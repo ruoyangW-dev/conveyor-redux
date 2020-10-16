7f306cf7321baf510ea20faed3d7bd993c18b876
Chris Carson <chriscarson81@gmail.com>
2020-10-16 11:20:42 -0700

    exporting all required methods from the index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
-import { ConveyorReducer } from './reducers'
-import { ConveyorEpic } from './epics'
-import { mergeConveyorActions } from './utils/mergeActions'
-
-export { ConveyorReducer }
-export { ConveyorEpic }
-export { mergeConveyorActions }
+export { ConveyorEpic, combineEpicsAndCatchErrors } from './epics'
+export { AlertEpic } from './epics/alert'
+export { CreateEpic } from './epics/create'
+export { EditEpic } from './epics/edit'
+export { IndexTableEpic } from './epics/indexTable'
+export { ModalEpic } from './epics/modal'
+export { ModelEpic } from './epics/model'
+export { OptionsEpic } from './epics/options'
+export { RouteEpic } from './epics/route'
+export { SearchEpic } from './epics/search'
+export { TooltipEpic } from './epics/tooltip'
+export { ValidationEpic } from './epics/validation'
+export { ConveyorReducer } from './reducers'
+export { AlertsReducer } from './reducers/alerts'
+export { CreateReducer } from './reducers/create'
+export { EditReducer } from './reducers/edit'
+export { ModelReducer } from './reducers/model'
+export { SearchReducer } from './reducers/search'
+export { selectAlerts } from './utils/alerts'
+export { handleDetailCreate } from './utils/create'
+export { selectEdit } from './utils/edit'
+export { mergeConveyorActions } from './utils/mergeActions'
+export { selectModal } from './utils/modal'
+export {
+  getPaginatedNode,
+  getTabIdentifier,
+  getModelStore,
+  getOrderedValues,
+  selectModel
+} from './utils/model'
+export { selectOptions, filterSelectOptions } from './utils/options'
+export {
+  selectSearchDropdown,
+  selectSearchQueryText,
+  selectSearchEntries
+} from './utils/search'
+export { selectTableView } from './utils/tableView'
+export { selectTooltip } from './utils/tooltip'
